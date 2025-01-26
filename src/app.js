e933a9b17a1e3ae07be1833b9a49257591fd6b63
martinSabatini <146122086+martinSabatini@users.noreply.github.com>
2025-01-26 18:00:35 +0000

    randomcards

--- src/app.js
+++ src/app.js
@@ -7,20 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function cards() {
   let cardIcons = ["♦", "♥", "♠", "♣"];
-  let cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let cardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   function getRandomIcons() {
-    let topIcon = cardIcons[Math.floor(Math.random() * cardIcons.length)];
-    let bottomIcon = cardIcons[Math.floor(Math.random() * cardIcons.length)];
+    let Icon = cardIcons[Math.floor(Math.random() * cardIcons.length)];
     let topElement = document.getElementById("topIcon");
     let bottomElement = document.getElementById("bottomIcon");
-    topElement.innerHTML = topIcon;
-    bottomElement.innerHTML = bottomIcon;
-    if (topIcon === "♦" || topIcon === "♥") {
+    topElement.innerHTML = Icon;
+    bottomElement.innerHTML = Icon;
+    if (Icon === "♦" || Icon === "♥") {
       topElement.style.color = "red";
     } else {
       topElement.style.color = "black";
     }
-    if (bottomIcon === "♦" || bottomIcon === "♥") {
+    if (Icon === "♦" || Icon === "♥") {
       bottomElement.style.color = "red";
     } else {
       bottomElement.style.color = "black";
